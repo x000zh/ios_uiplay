24a0c8965b6a5af0180853d8b6e696a372f0ba56
Zhang Xiang <zhangxiang@acfun.tv>
2017-04-07 14:02:13 +0800

    Initial Commit

---
+++ uiplay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  uiplay
+//
+//  Created by Zhang Xiang on 2017/4/7.
+//  Copyright © 2017年 zhangxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ uiplay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  uiplay
+//
+//  Created by Zhang Xiang on 2017/4/7.
+//  Copyright © 2017年 zhangxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
